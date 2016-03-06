71ea17b7fa3133f26828c0b5858c0542d4192c8d
Tim Leerhoff <tleerhof@web.de>
2016-03-06 15:00:22 +0100

    Added tests for init and shutdown order in models.

--- test/scenario.js
+++ test/scenario.js
@@ -2,22 +2,24 @@ var assert = require('assert');
 var sim = require((process.env.APP_DIR_FOR_CODE_COVERAGE || '../lib/') + 'scenario.js');
 
 
-var scnDef = {
-  models: []
-};
 
-
-var TestModel = function() {
+var TestModel = function(name, initArray, shutdownArray) {
   sim.EmptyModel.call(this);
+  this.name = name;
+  initArray = initArray || [];
+  shutdownArray = shutdownArray || [];
+
   this.initCalled = false;
   this.shutdownCalled = false;
 
   this.init = function(api) {
     this.initCalled = true;
+    initArray.push(this);
   },
 
   this.shutdown = function(api) {
     this.shutdownCalled = true;
+    shutdownArray.push(this);
   }
 };
 TestModel.prototype = new sim.EmptyModel();
@@ -28,7 +30,7 @@ TestModel.prototype = new sim.EmptyModel();
 describe('Scenario', function () {
   describe('when a model is registered in scenario', function () {
     it('should call init', function () {
-      var modelTree = new TestModel();	
+      var modelTree = new TestModel('n1');	
       var scenario = new sim.Scenario();
       scenario.setModelTree(modelTree);
       scenario.doPendingInits();
@@ -74,6 +76,46 @@ describe('Scenario', function () {
       scenario.doPendingShutdowns();
 
       assert.equal(true, newModel.shutdownCalled);
+    });
+  });
+
+  describe('when a modeltree is registered to a scenario', function () {
+    it('should call init in right order', function () {
+      var initArray = [];
+      var shutdownArray = []; 
+      var modelTree = new TestModel('n1', initArray, shutdownArray);	
+      var newSubModel1 = new TestModel('n2', initArray, shutdownArray);
+      var newSubModel2 = new TestModel('n3', initArray, shutdownArray);
+      var scenario = new sim.Scenario();
+      modelTree.addChild(newSubModel1);
+      newSubModel1.addChild(newSubModel2);
+      scenario.setModelTree(modelTree);
+
+      scenario.doPendingInits();
+
+      assert.equal(modelTree, initArray[0]);
+      assert.equal(newSubModel1, initArray[1]);
+      assert.equal(newSubModel2, initArray[2]);
+    });
+
+    it('should call shutdown in right order', function () {
+      var initArray = [];
+      var shutdownArray = []; 
+      var modelTree = new TestModel('n1', initArray, shutdownArray);	
+      var newSubModel1 = new TestModel('n2', initArray, shutdownArray);
+      var newSubModel2 = new TestModel('n3', initArray, shutdownArray);
+      var scenario = new sim.Scenario();
+      modelTree.addChild(newSubModel1);
+      newSubModel1.addChild(newSubModel2);
+      scenario.setModelTree(modelTree);
+      scenario.doPendingInits(); 
+ 
+      scenario.clear();
+      scenario.doPendingShutdowns();
+
+      assert.equal(newSubModel2, shutdownArray[0]);
+      assert.equal(newSubModel1, shutdownArray[1]);
+      assert.equal(modelTree, shutdownArray[2]);
     });
   });
 
